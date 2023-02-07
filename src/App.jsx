e534bd626534beec63e773c07a61a6e24b7195cb
Gabriel Garcias <alexandroggm@gmail.com>
2023-02-07 11:52:29 -0400

    Reorganiza estructura de carpetas del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { API_URL } from "../env"
 
 const App = () => {
   
+  const [data, setData] = useState({})
+  //User admin del curso
+  const user = { "email": "beto@ed.team",
+  "password": "123abc"
+  }
+
+  useEffect(()=>{
+    axios.get(`${API_URL}/public/products`)
+    .then(d => setData(d.data))
+    .catch( e => console.error(e))
+  },[])
+  console.log(data)
   return (
     <div>
       <h1>Hola chanchito{API_URL}</h1>
