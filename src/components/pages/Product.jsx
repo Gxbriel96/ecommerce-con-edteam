fe15321ce3abb0deb1e49c18d535766f4c8faf1d
Gabriel Garcias <alexandroggm@gmail.com>
2023-04-17 19:30:31 -0400

    modificado en linux, CartContext, CartReducer agregado, agregado botones para el carrito

---
+++ src/components/pages/Product.jsx
@@ -0,0 +1,51 @@
+import axios from "axios"
+import { useContext, useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { API_URL } from "../../../env"
+import { CartContext } from "../../context/CartContext"
+
+const Product = () => {
+  const { dispatch } = useContext(CartContext)
+  const params = useParams()
+  const [product, setProduct] = useState()
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/public/products/${params.id}`)
+      .then((resp) => {
+        setProduct(resp.data.data)
+        console.log(product)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  const addToCart = () => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: product
+    })
+  }
+  const removeFromCart = () => {
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: product
+    })
+  }
+
+  if (!product) return <h1>CARGANDO...</h1>
+
+  return (
+    <div>
+      <h1 className="text-3xl">Producto: {product.product_name}</h1>
+      <p>{JSON.stringify(product)}</p>
+      <button className="bg-gradient" onClick={addToCart}>
+        AGREGAR AL CARRITO
+      </button>
+      <button className="bg-gradient" onClick={removeFromCart}>
+        QUITAR DEL CARRITO
+      </button>
+    </div>
+  )
+}
+
+export default Product
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
+import { CartProvider } from "./context/CartContext"
 import { UserProvider } from "./context/UserContext"
 import RouterG from "./router/RouterG"
 import "./styles/index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <UserProvider>
-    <RouterProvider router={RouterG} />
-  </UserProvider>
+  <CartProvider>
+    <UserProvider>
+      <RouterProvider router={RouterG} />
+    </UserProvider>
+  </CartProvider>
 )
